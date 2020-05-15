cc00d4203b80147191d585077e265aa3a05bb592
luoxuetong <luoxuetong@9fbank.com.cn>
2020-05-15 17:42:52 +0800

    初始提交

---
+++ src/main/java/com/allen/turbine/TurbineServerApplication.java
@@ -0,0 +1,13 @@
+package com.allen.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineServerApplication.class, args);
+	}
+
+}
