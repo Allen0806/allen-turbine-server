9c48068245133879508fe5898c6096954882f1b5
luoxuetong <luoxuetong@9fbank.com.cn>
2020-05-19 09:07:49 +0800

    修改配置文件

--- src/main/java/com/allen/turbine/TurbineServerApplication.java
+++ src/main/java/com/allen/turbine/TurbineServerApplication.java
@@ -2,8 +2,10 @@ package com.allen.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine
 public class TurbineServerApplication {
 
 	public static void main(String[] args) {
